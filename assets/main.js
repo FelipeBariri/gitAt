c255e87106f5ab3e702b9b30bd5a0b1baf6efffc
FelipeBariri <ephzip@gmail.com>
2023-08-04 11:35:42 -0300

    versão final

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
+var imgAtual = "img2.jpeg";
+var imgAnterior = "img1.jpeg";
+
+function trocar(){
+    document.getElementById("figura").src = 'assets/' +imgAtual;
+    let aux = imgAtual
+    imgAtual = imgAnterior
+    imgAnterior = aux;
+}
+
 function alterarCor() {
     seletor = document.querySelector("input");
     document.body.style.backgroundColor = seletor.value;
