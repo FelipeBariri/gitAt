9adc9a9d01d1f70ef617f830e61502c31961e731
FelipeBariri <ephzip@gmail.com>
2023-08-04 11:04:56 -0300

    atualização 2.0

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,4 @@
-document.addEventListener("click", trocaCor );
-
-document.getElementById("btn").addEventListener("click", trocaNome );
-
-function trocaNome(e)
-{
-    let nome = document.getElementById( "nome" ).value
-    document.getElementById( "frase" ).innerHTML = "Meu nome é " + nome;
-    e.preventdefault();
-}
-function trocaCor( e ) {
-    document.body.style.backgroundColor= 'lightblue' ;
+function alterarCor() {
+    seletor = document.querySelector("input");
+    document.body.style.backgroundColor = seletor.value;
 }
